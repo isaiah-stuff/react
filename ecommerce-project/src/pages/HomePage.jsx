ac320975bf1d681588c4783ccb54d02388be085b
isaiah-stuff <stuffleisaiah@gmail.com>
2026-05-03 09:03:06 -0400

    Use data from the backend to generate html

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,13 +1,22 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header.jsx";
 import CheckmarkIcon from "../assets/images/icons/checkmark.png";
-import { products } from "../../data/products.js";
 import "./HomePage.css";
 
 export function HomePage() {
-  axios.get("http://localhost:3000/api/products").then((response) => {
-    console.log(response.data);
-  });
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/products")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("API error: ", error);
+      });
+  }, []);
 
   return (
     <>
