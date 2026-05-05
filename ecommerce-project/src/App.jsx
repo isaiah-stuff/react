bd329415dcdca24db6c8bcfb0e18671e7846ca87
isaiah-stuff <stuffleisaiah@gmail.com>
2026-05-05 14:39:01 -0400

    Generate HTML for the cart items

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Routes, Route } from "react-router";
 import { useState, useEffect } from "react";
 import { HomePage } from "./pages/HomePage";
-import { CheckoutPage } from "./pages/CheckoutPage";
+import { CheckoutPage } from "./pages/checkout/CheckoutPage";
 import { OrdersPage } from "./pages/OrdersPage";
 import { TrackingPage } from "./pages/TrackingPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
@@ -14,7 +14,7 @@ function App() {
 
   useEffect(() => {
     axios
-      .get("api/cart-items")
+      .get("api/cart-items?expand=product")
       .then((response) => {
         setCart(response.data);
       })
